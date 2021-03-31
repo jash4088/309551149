8ca9846cf79add5d6b60f0a45ee6545cafbca79d
jash4088 <30929992+jash4088@users.noreply.github.com>
2021-04-01 03:28:01 +0800

    Initial commit

---
+++ src/main/java/testTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class testTestTest {
+    static Stream<Arguments> stringIntAndListProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
+                Arguments.of(new int[]{-1, -4, -2, 0}, new int[]{-4, -2, -1, 0}),
+                Arguments.of(new int[]{0, 0, 0, 0}, new int[]{0, 0, 0, 0}),
+                Arguments.of(new int[]{-1}, new int[]{-1}),
+                Arguments.of(new int[]{2147483647, 0, -2147483648}, new int[]{-2147483648, 0, 2147483647})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringIntAndListProvider")
+    public void Runtest(int[] input, int[] answer) {
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+        int index = 0;
+        int i = 0, j = 0;
+        int[] result = new int[input.length];
+
+        while(i < result.length)
+        {
+            q.add(input[i++]);
+        }
+
+        while(j < result.length)
+        {
+            result[j++] = q.remove();
+        }
+
+        assertArrayEquals(answer, result);
+    }
+
+    @Test
+    void Expection_IllegalArgumentException()
+    {
+        Exception exc = assertThrows(IllegalArgumentException.class, () -> {
+            PriorityQueue<Integer> q = new PriorityQueue<Integer>( -2);
+        });
+    }
+
+    @Test
+    void Expection_NullPointerException()
+    {
+        Exception exc = assertThrows(NullPointerException.class, () -> {
+            PriorityQueue<Integer> q = new PriorityQueue<Integer>( 1);
+            q.add(null);
+        });
+    }
+
+    @Test
+    void Expection_NoSuchElementException()
+    {
+        Exception exc = assertThrows(NoSuchElementException.class, () -> {
+            PriorityQueue<String> q = new PriorityQueue<String>( 1);
+            q.remove();
+            q.remove();
+        });
+    }
+
+
+}
